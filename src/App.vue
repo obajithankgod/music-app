b838dffefa37df3636195bb03dbafd4374e2865c
Obaji Thankgod <obajithankgod360@gmail.com>
2023-08-09 18:41:42 +0100

    added some songs

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,38 @@
 <template>
- <div class="app">
-   <header>
-     <h1>Music App</h1>
-   </header>
-   <main>
-     <section class="player">
-        <h1 class="song-title">{{ current.artist }} -- <span>{{ current.title }}</span></h1>
-       <div class="controls">
-         <button class="prev" @click="prev">prev</button>
-         <button class="play" v-if="!isPlaying" @click="play">play</button>
-         <button class="pause" v-else @click="pause">pause</button>
-         <button class="next" @click="next">next</button>
-       </div>
-     </section>
-     <section class="playlist">
-       <h3>My Playlist</h3>
-       <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
-         {{ song.title }} - {{ song.artist }}
-
-       </button>
-
-     </section>
-   </main>
-   
- </div>
+  <div class="app">
+    <header>
+      <h1>Music App</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h1 class="song-title">
+          {{ current.artist }} -- <span>{{ current.title }}</span>
+        </h1>
+        <div class="controls">
+          <button class="prev" @click="prev">prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">play</button>
+          <button class="pause" v-else @click="pause">pause</button>
+          <button class="next" @click="next">next</button>
+        </div>
+      </section>
+      <section class="playlist">
+        <h3>My Playlist</h3>
+        <button
+          v-for="song in songs"
+          :key="song.src"
+          @click="play(song)"
+          :class="song.src == current.src ? 'song playing' : 'song'"
+        >
+          {{ song.title }} - {{ song.artist }}
+        </button>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       current: {},
@@ -36,79 +40,99 @@ export default {
       isPlaying: false,
       songs: [
         {
-          title:'Godly',
-          artist: 'Omah_lay',
-          src: require('./assets/Omah_Lay_-_Godly.mp3')
+          title: "Godly",
+          artist: "Omah_lay",
+          src: require("./assets/Omah_Lay_-_Godly.mp3"),
         },
         {
-          title: 'Hello_Brother',
-          artist: 'Omah_lay',
-          src: require('./assets/Omah_Lay_-_Hello_Brother(128k).mp3')
+          title: "Faaji",
+          artist: "Joeboy_Ft_Mr_Eazi",
+          src: require("./assets/Joeboy_Ft_Mr_Eazi_Faaji_9jaflaver.com_.mp3"),
         },
         {
-          title: 'Bad-influence',
-          artist: 'Omah_lay',
-          src: require('./assets/Omah-Lay-Bad-Influence-(JustNaija.com).mp3')
-        }
-        
+          title: "Hello_Brother",
+          artist: "Omah_lay",
+          src: require("./assets/Omah_Lay_-_Hello_Brother(128k).mp3"),
+        },
+        {
+          title: "Bad-influence",
+          artist: "Omah_lay",
+          src: require("./assets/Omah-Lay-Bad-Influence-(JustNaija.com).mp3"),
+        },
+        {
+          title: "Forever",
+          artist: "Gyakie",
+          src: require("./assets/Gyakie-Forever-(TrendyBeatz.com).mp3"),
+        },
+        {
+          title: "wait_and_see",
+          artist: "Fireboy_DML",
+          src: require("./assets/Fireboy_DML_Wait_And_See_9jaflaver.com_-1.mp3"),
+        },
+        {
+          title: "You",
+          artist: "Fireboy_DML",
+          src: require("./assets/Fireboy_DML_You_9jaflaver.com_.mp3"),
+        },
+        {
+          title: "God_Only_Knows",
+          artist: "Fireboy",
+          src: require("./assets/Fireboy_God_Only_Knows_9jaflaver.com_.mp3"),
+        },
       ],
-      player: new Audio()
-    }
+      player: new Audio(),
+    };
   },
 
   methods: {
-    play (song) {
-      if (typeof song.src !="undefined") {
+    play(song) {
+      if (typeof song.src != "undefined") {
         this.current = song;
 
         this.player.src = this.current.src;
       }
 
       this.player.play();
-      this.player.addEventListener('ended', function () {
-        this.index++;
-        if (this.index > this.songs.length - 1) {
-        this.index = 0;
-      }
+      this.player.addEventListener(
+        "ended",
+        function () {
+          this.index++;
+          if (this.index > this.songs.length - 1) {
+            this.index = 0;
+          }
 
-      this.current = this.songs[this.index];
-      this.play(this.current);
-
-      }.bind(this))
+          this.current = this.songs[this.index];
+          this.play(this.current);
+        }.bind(this)
+      );
       this.isPlaying = true;
     },
-    pause () {
+    pause() {
       this.player.pause();
       this.isPlaying = false;
     },
-    next () {
+    next() {
       this.index++;
       if (this.index > this.songs.length - 1) {
         this.index = 0;
       }
 
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev() {
+      this.index--;
+      if (this.index < 0) this.index = this.songs.length - 1;
 
       this.current = this.songs[this.index];
       this.play(this.current);
-
     },
-    prev () {
-      this.index--;
-      if (this.index < 0)
-      this.index = this.songs.length - 1;
-
-      this.current = this.songs[this.index];
-      this.play(this.current)
-
-    }
-
   },
-  created () {
-        this.current = this.songs[this.index];
-        this.player.src = this.current.src;
-    }
-  
-}
+  created() {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+  },
+};
 </script>
 
 <style>
@@ -121,33 +145,32 @@ body {
   font-family: sans-serif;
   background: linear-gradient(35deg, #ccffff, #ffcccc);
 }
- header {
-   display: flex;
-   justify-content: center;
-   align-content: center;
-   padding: 15px;
-   background-color: #212121;
-   color: #fff ;
-    
- }
- main {
-   width: 100%;
-   max-width: 768px;
-   margin: 0 auto;
-   padding: 25px;
- }
- .song-title {
-   color: #9591b1;
-   font-size: 20px;
-   font-weight: 700;
-   text-transform: uppercase;
-   text-align: center;
- }
- .song-title span {
-   font-weight: 400;
-   font-style: italic;
-   font-size: 20px;
- }
+header {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #fff;
+}
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
+}
+.song-title {
+  color: #9591b1;
+  font-size: 20px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+  font-size: 20px;
+}
 
 .controls {
   display: flex;
@@ -162,12 +185,12 @@ button {
   border: none;
   outline: none;
   cursor: pointer;
-  
 }
-button:hover{
+button:hover {
   opacity: 0.8;
 }
-.play, .pause {
+.play,
+.pause {
   font-size: 20px;
   font-weight: 700;
   padding: 15px 25px;
@@ -175,9 +198,10 @@ button:hover{
   border-radius: 8px;
   color: #fff;
   background-color: #cc2e5d;
- box-shadow: 1px 1px 10px lightblue;
+  box-shadow: 1px 1px 10px lightblue;
 }
-.next, .prev {
+.next,
+.prev {
   font-size: 15px;
   font-weight: 700;
   padding: 10px 20px;
@@ -185,8 +209,7 @@ button:hover{
   border-radius: 6px;
   color: #fff;
   background-color: #011b2c;
-  box-shadow: 1px 1px  5px lightblue;
-
+  box-shadow: 1px 1px 5px lightblue;
 }
 .playlist {
   padding: 0px 15px;
@@ -197,7 +220,6 @@ button:hover{
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   font-weight: 400;
   margin-bottom: 30px;
- 
 }
 .playlist .song {
   display: block;
@@ -217,3 +239,4 @@ button:hover{
   color: #ff5858;
 }
 </style>
+
